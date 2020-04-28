344d2bb96d4bf24de669fc9dbfec3c96479b9a47
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-04-28 15:37:56 -0400

    Added login component and set up route for home page of app

---
+++ src/components/RecipeContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class RecipeContainer extends React.Component {
+  render() {
+    return (
+      <div>Recipe Container</div>
+    )
+  }
+}
+
+export default RecipeContainer
---
+++ src/components/login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Login extends React.Component {
+  render() {
+    return(
+      <div>Login Component</div>
+
+    )
+  }
+}
+
+export default Login
