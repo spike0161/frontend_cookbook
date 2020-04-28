17cb61ac0e4f08925090520fd959e0adcb980542
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-04-28 15:36:56 -0400

    Created login component and created / route to log into the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch } from 'react-router-dom'
+import RecipeContainer from "./components/RecipeContainer";
+import Login from './components/login'
 
-function App() {
-  return (
-    <div>
-      Cookbook
-    </div>
-  );
+class App extends React.Component {
+
+  // componentDidMount
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route  exact path='/' component={Login}/>
+        </Switch>
+        </div>
+    );
+  }
 }
 
 export default App;
