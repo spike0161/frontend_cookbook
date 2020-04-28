e3dc582e2451bdb29a420a8b484568f39be6d6de
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-04-28 16:54:32 -0400

    dispatched recipe array to get all recipes

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,14 +1,19 @@
+const FETCHED_RECIPES = "FETCHED_RECIPES";
 
-const FETCHED_RECIPES = "FETCHED_RECIPES"
+const URL = "http://localhost:3000/recipes";
 
-const URL = "http://localhost:3000/recipes"
-
-function fetchingRecipes(){
-  return (dispatch) => {
-  fetch(URL)
-  .then(res => res.json())
-  .then(recipe_array => console.log(recipe_array))
-  }
+function fetchedRecipes(recipe_array) {
+  return { type: FETCHED_RECIPES, payload: recipe_array };
 }
 
-export { fetchingRecipes }
+function fetchingRecipes() {
+  return dispatch => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(recipe_array => {
+        dispatch(fetchedRecipes(recipe_array));
+      });
+  };
+}
+
+export { fetchingRecipes, FETCHED_RECIPES };
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,21 @@
+import { combineReducers } from 'redux'
+import { FETCHED_RECIPES } from './actionCreators'
 
+// function reducer(state, action){
+//     return state
+// }
 
-function reducer(state, action){
-    return state
+const recipeReducer = (oldState=[], action) => {
+  switch (action.type) {
+    case FETCHED_RECIPES:
+      return action.payload
+    default:
+      return oldState
+  }
 }
 
-export  default reducer
+const rootReducer = combineReducers({
+  recipes: recipeReducer
+})
+
+export default rootReducer
