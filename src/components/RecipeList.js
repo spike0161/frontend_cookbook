639a81a4e6895b684624fbfa03b3ff079430e9bc
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-04-30 11:33:55 -0400

    Filtered recipes from searchbar and changed store state

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,18 +3,17 @@ import RecipeListItem from "./RecipeListItem";
 import { connect } from "react-redux";
 
 const RecipeList = props => {
-  console.log("In recipeList:", props.recipes);
   return (
     <div>
       {props.recipes.map(recipe => (
-        <RecipeListItem recipe={recipe} key={recipe.id}/>
+        <RecipeListItem recipe={recipe} key={recipe.id} />
       ))}
     </div>
   );
 };
 
 const mapStateToProps = store => ({
-   recipes: store.recipes
+  recipes: store.recipes.filter(recipe => recipe.title.toLowerCase().includes(store.searchText.toLowerCase()))
 });
 
 export default connect(mapStateToProps)(RecipeList);
