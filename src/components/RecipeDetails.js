a8c411e81975e12a3cd0450b7ff7a9a4fcfa776c
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-05-01 10:42:50 -0400

    Fleshing out the information on recipe details page

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -4,15 +4,27 @@ import {withRouter} from 'react-router-dom'
 
 class RecipeDetails extends React.Component {
   render() {
+    console.log("RecipeDetails props:", this.props);
     return !this.props.recipe ? null : (
 
       <div>RecipeDetails
         <h3>{this.props.recipe.title}</h3>
+        <p>Dairy Free: {this.props.recipe.dairy_free}</p>
+        <p>Gluten Free: {this.props.recipe.gluten_free}</p>
+        <p>Vegan: {this.props.recipe.vegan}</p>
+        <p>Vegetarian: {this.props.recipe.vegetarian}</p>
+
+
+
+        <img src={this.props.recipe.picture} alt="recipe-picture"/>
+        <p>Cook Time: {this.props.recipe.cook_time} mins</p>
+        <h4>Instructions:</h4>        <p>{this.props.recipe.instructions}</p>
+
       </div>
     )
   }
 }
-
+// rating, healthscore
 
 
 const mapStateToProps = (store, ownProps) => ({
