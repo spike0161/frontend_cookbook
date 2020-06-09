3f8c546b3b08fdfd21761132ae586ee642d81d49
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-06-09 15:56:37 -0400

    Styled the reviews div

--- src/components/UserProfilePage.js
+++ src/components/UserProfilePage.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter, Link } from "react-router-dom";
-import { removeFavRecipe } from '../redux/actionCreators'
+import { removeFavRecipe } from "../redux/actionCreators";
 
 const UserProfilePage = props => {
-  console.log("In userProfile", props)
+  console.log("In userProfile", props);
   return (
     <div className="user-profile-div">
       <div className="profile-div">
-      <img id="avatar" src="https://st3.depositphotos.com/4111759/13425/v/450/depositphotos_134255626-stock-illustration-avatar-male-profile-gray-person.jpg" alt="avatar"/>
-      <h3>{props.user.user.username}</h3>
-      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <img
+          id="avatar"
+          src="https://st3.depositphotos.com/4111759/13425/v/450/depositphotos_134255626-stock-illustration-avatar-male-profile-gray-person.jpg"
+          alt="avatar"
+        />
+        <h3>{props.user.user.username}</h3>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
       </div>
       My Favorite Recipes:{" "}
       <div className="container-fluid">
@@ -18,21 +30,28 @@ const UserProfilePage = props => {
           {props.user.favorites.map(recipe => (
             <div className="col-lg-2" key={recipe.id}>
               <div className="card">
-                <img
-                  className="card-img-top"
-                  src={recipe.picture}
-                  alt="Card"
-                />
+                <img className="card-img-top" src={recipe.picture} alt="Card" />
                 <div className="card-body">
                   <h5 className="card-title">{recipe.title}</h5>
 
-                      <div class="btn-group" role="group" aria-label="Basic example">
-                        <Link to={`/recipes/${recipe.id}`}><button type="button" class="btn btn-secondary">See Recipe</button>
-                      </Link>
-                        <button type="button delete-fav" class="btn btn-secondary" onClick={e => props.deleteFavHandler(recipe, props.user)}>x</button>
-                      </div>
-
-
+                  <div
+                    class="btn-group"
+                    role="group"
+                    aria-label="Basic example"
+                  >
+                    <Link to={`/recipes/${recipe.id}`}>
+                      <button type="button" class="btn btn-secondary">
+                        See Recipe
+                      </button>
+                    </Link>
+                    <button
+                      type="button delete-fav"
+                      class="btn btn-secondary"
+                      onClick={e => props.deleteFavHandler(recipe, props.user)}
+                    >
+                      x
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -51,7 +70,12 @@ const mapStateToProps = store => ({
 const mapDispatchToProps = dispatch => {
   return {
     deleteFavHandler: (recipe, user) => dispatch(removeFavRecipe(recipe, user))
-  }
-}
+  };
+};
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(UserProfilePage));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(UserProfilePage)
+);
