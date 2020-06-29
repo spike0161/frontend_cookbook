f4ca8b304d480abfc9029290aafb4ae50403cfda
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-06-29 13:51:01 -0400

    Fixed instructions with regex characters

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -9,37 +9,51 @@ class RecipeDetails extends React.Component {
     return !this.props.recipe ? null : (
       <div className="recipe-details">
         <h3>{this.props.recipe.title}</h3>
-        {!this.props.recipe.ingredients ? null :
-        this.props.recipe.ingredients.map(ing => (
-          <p key={ing.id}>
-            {ing.name} {ing.amount} {ing.unit}
-          </p>
-        ))
-      }
+        {!this.props.recipe.ingredients
+          ? null
+          : this.props.recipe.ingredients.map(ing => (
+              <p key={ing.id}>
+                {ing.name} {ing.amount} {ing.unit}
+              </p>
+            ))}
         <img src={this.props.recipe.picture} alt="recipe" />
         <p>Cook Time: {this.props.recipe.cook_time} mins</p>
-          <div id='dietary'>
-            <p>Dairy Free: {this.props.recipe.dairy_free ? "Yes" : "No"}</p>
-            <p>Gluten Free: {this.props.recipe.gluten_free ? "Yes" : "No"}</p>
-            <p>Vegan: {this.props.recipe.vegan ? "Yes" : "No"}</p>
-            <p>Vegetarian: {this.props.recipe.vegetarian ? "Yes" : "No"}</p>
-          </div>
-        <h4>Recipe Preparation:</h4> <p>{this.props.recipe.instructions}</p>
-          <button className="btn btn-primary btn-lg btn-block login-btn" id="fav-btn"
-            onClick={() =>
-              this.props.fav(this.props.recipe, this.props.user.user)
-            }
-          >
-            Favorite Recipe
-          </button>
+        <div id="dietary">
+          <p>Dairy Free: {this.props.recipe.dairy_free ? "Yes" : "No"}</p>
+          <p>Gluten Free: {this.props.recipe.gluten_free ? "Yes" : "No"}</p>
+          <p>Vegan: {this.props.recipe.vegan ? "Yes" : "No"}</p>
+          <p>Vegetarian: {this.props.recipe.vegetarian ? "Yes" : "No"}</p>
+        </div>
+        <h4>Recipe Preparation:</h4>
+        {!this.props.recipe.instructions ? (
+          "No instructions available..."
+        ) : (
+          <p>
+            {this.props.recipe.instructions.replace(
+              /(<li>|<ol>|<\/ol>|<\/li>)/g,
+              ""
+            )}
+          </p>
+        )}
+        <button
+          className="btn btn-primary btn-lg btn-block login-btn"
+          id="fav-btn"
+          onClick={() =>
+            this.props.fav(this.props.recipe, this.props.user.user)
+          }
+        >
+          Favorite Recipe
+        </button>
         <ReviewForm />
         {this.props.recipe.reviews
           ? this.props.recipe.reviews.map(rev => (
               <div id="review-div" key={rev.id}>
-                Rating : {rev.rating} / 5
-                <h4>{rev.review}</h4>
+                Rating : {rev.rating} / 5<h4>{rev.review}</h4>
                 {rev.user_id !== this.props.user.user.id ? null : (
-                  <button id="delete-review-btn"onClick={() => this.props.deleteReview(rev)}>
+                  <button
+                    id="delete-review-btn"
+                    onClick={() => this.props.deleteReview(rev)}
+                  >
                     x
                   </button>
                 )}
